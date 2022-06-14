c6c1203fd4959ed9911aeda7ddcef3a8f80bb1c5
atarikaltunn <atarikaltunn@gmail.com>
2022-06-14 16:08:03 +0300

    User model, creation route and controller  added, encryption applied

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const pageRoute = require('./routes/pageRoutes');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -25,6 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
