6dfd6af1451133c1dbfb70603e001d8eccf6b444
atarikaltunn <atarikaltunn@gmail.com>
2022-06-16 15:34:22 +0300

    Login page and login process added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
@@ -6,6 +7,28 @@ exports.createUser = async (req, res) => {
         res.status(201).json({
             status: 'success',
             user,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        await User.findOne({ email }, (err, user) => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, same) => {
+                    if (same) {
+                        // USER SESSION
+                        res.status(200).send('YOU ARE LOGGED IN');
+                    }
+                });
+            }
         });
     } catch (error) {
         res.status(400).json({
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); //post to localhost:3000/user/signup
+router.route('/login').post(authController.loginUser); //post to localhost:3000/user/signup
 
 
 module.exports = router;
